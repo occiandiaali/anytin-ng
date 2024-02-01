38db0c43bb66d3e45ec013e8a4c4570c8ba3ca48
occiandiaali <ocean.diaali@gmail.com>
2024-02-01 13:22:35 +0100

    add form module - test

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatTooltipModule, TextFieldModule],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
