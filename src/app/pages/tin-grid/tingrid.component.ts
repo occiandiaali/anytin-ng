d0b1b3edc020cc8f5749ebf27fea012c9c70e86b
occiandiaali <ocean.diaali@gmail.com>
2024-09-06 22:26:03 +0100

    Add api calls for dummy products list

--- src/app/pages/tin-grid/tingrid.component.ts
+++ src/app/pages/tin-grid/tingrid.component.ts
@@ -1,12 +1,136 @@
-import { Component } from '@angular/core';
+// import { Component, OnInit, inject } from '@angular/core';
+// import { TinService } from '../../services/tin.service';
+// import { DatePipe, NgOptimizedImage } from '@angular/common';
+// import { RouterModule } from '@angular/router';
+// import { Observable, catchError, finalize } from 'rxjs';
+// import { ApiResult, Product } from '../../services/interfaces';
+
+// @Component({
+//   selector: 'app-tingrid',
+//   standalone: true,
+//   imports: [DatePipe, NgOptimizedImage, RouterModule],
+//   templateUrl: './tingrid.component.html',
+//   styleUrl: './tingrid.component.scss'
+// })
+// export class TinGridComponent implements OnInit {
+//   private tinService = inject(TinService);
+
+//   currentShow = 8;
+//   public products: Product[] = [];
+//   values: any[] = [];
+//   public isLoading = true;
+//   public error = null;
+//   public dummyArray = new Array(8);
+//   placeholder = 'PLACEHOLDER_CONSTANT';
+
+//   allProducts$ = Observable<any[]>;
+
+
+//   ngOnInit(): void {
+//     //  this.loadProducts();
+//     //  this.showAllProducts();
+//     this.tinService.allListings().subscribe({
+//       next: (listings) => {
+//         this.values = listings.results;
+//         console.log(`Listings: ${this.values}`);
+//       },
+//       error: (error) => {
+//         this.error = error;
+//       }
+//     });
+//   }
+
+
+//   async showAllProducts() {
+//     this.error = null;
+//     this.tinService.allProducts$.subscribe(data => this.products.push(...data.results));
+//   }
+
+//   async loadProducts() {
+//     this.error = null;
+
+//     this.tinService
+//       .getAllProducts(this.currentShow)
+//       .pipe(
+//         finalize(() => {
+//           this.isLoading = false;
+//         }),
+//         catchError((err: any) => {
+//           this.error = err.error.status_message;
+//           return [];
+//         })
+//       )
+//       .subscribe({
+//         next: (res) => {
+//           const arr = res;
+//           console.log("Res: ", res);
+//           console.log("Arr: ", Object.values(arr));
+//           this.values = Object.values(arr);
+//           console.log("Products: ", this.values);
+//           this.products.push(...this.values);
+//          // console.log(`this.products => ${JSON.stringify(this.products)}`);
+//           const v = JSON.stringify(this.products);
+//           console.log("V ", JSON.parse(v));
+
+//         // this.products.push(...res.results);
+//         }
+//       })
+//   }
+// }
+
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+
+import { TinService } from '../../services/tin.service';
+import { map, Observable } from 'rxjs';
+import { Product } from '../../services/interfaces';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-tingrid',
+  templateUrl: 'tingrid.component.html',
+  styleUrls: ['tingrid.component.scss'],
   standalone: true,
-  imports: [],
-  templateUrl: './tingrid.component.html',
-  styleUrl: './tingrid.component.scss'
+  imports: [AsyncPipe, CommonModule, ProductCardComponent],
 })
-export class TinGridComponent {
+export class TinGridComponent implements OnInit {
+  #listingService = inject(TinService);
+  beauty$!: Observable<Product[]>;
+  furniture$!: Observable<Product[]>;
+  groceries$!: Observable<Product[]>;
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+      this.getAllBeauty();
+      this.getAllFurniture();
+      this.getAllGroceries();
+
+      
+  }
+
+  getAllBeauty() {
+          this.beauty$ = this.#listingService.getAll().pipe(
+        map((response: {products: Product[]}) => {
+          return response.products?.filter((listing: Product) => listing.category === "beauty")
+        })
+      );
+  }
+
+  getAllFurniture() {
+         this.furniture$ = this.#listingService.getAll().pipe(
+        map((response: {products: Product[]}) => {
+          return response.products?.filter((listing: Product) => listing.category === "furniture")
+        })
+      );
+  }
+
+    getAllGroceries() {
+    this.groceries$ = this.#listingService.getAll().pipe(
+        map((response: {products: Product[]}) => {
+          return response.products?.filter((listing: Product) => listing.category === "groceries")
+        })
+      );
+  }
 }
--- src/app/services/tin.service.ts
+++ src/app/services/tin.service.ts
@@ -1,9 +1,66 @@
-import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable, inject } from '@angular/core';
+// import { delay } from 'rxjs/operators';
+// import { Observable } from 'rxjs/internal/Observable';
+// import { ApiResult, Product } from './interfaces';
+
+// const BASE_URL = 'https://fakestoreapi.com/';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TinService {
+
+//   private http = inject(HttpClient);
+
+//   constructor() { }
+
+//   getAllProducts(limit = 8): Observable<ApiResult> {
+//     return this.http
+//       .get<ApiResult>(`${BASE_URL}products?limit=${limit}`)
+//       .pipe(
+//         delay(2000)
+//       );
+//   }
+//     getAllProducts(limit = 8): Observable<ApiResult> {
+//     return this.http
+//       .get<ApiResult>(`${BASE_URL}products?limit=${limit}`);
+//   }
+
+//   allListings(limit = 9): Observable<ApiResult> {
+//     return this.http.get<ApiResult>(`${BASE_URL}products?limit=${limit}`);
+//   }
+
+//   allProducts$ = this.http.get<ApiResult>(`${BASE_URL}products?limit=8`);
+
+//   getProductDetails(id: string): Observable<Product> {
+//     return this.http.get<Product>(`${BASE_URL}products/${id}`);
+//   }
+// }
+
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { Product } from './interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TinService {
+  #http = inject(HttpClient);
 
   constructor() { }
+
+  getAll(query = ''): Observable<any> {
+    return this.#http.get(
+      'https://dummyjson.com/product',
+    )
+  }
+  getAllByCategory(query = ''): Observable<any> {
+    return this.#http.get(
+      `https://dummyjson.com/products/category/${query}`
+    );
+  }
 }
